9d0f85484659c3c2421fe0b6200f8ba27a9b0b30
Halkano <kudezx@gmail.com>
2022-08-28 22:21:20 +0300

    completed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+function reverse(word){
+  return word.split("").reverse().join("");
+}
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseWord = reverse(word)
+  return word === reverseWord
 }
 
 /* 
   Add your pseudocode here
-*/
+  write afunction of isPalidnrome 
+  if "mom" === "mom"
+  returns true
+  else if "pot" === "top"
+  returns false
+  */
 
 /*
   Add written explanation of your solution here
+  I tried coming up with a function that has a code of reverse method and give a variable,
+   that in turn will be used by the isPalindrome function to campare the string argument
 */
 
 // You can run `node index.js` to view these console logs
